fae3f87d05efb9021a9c2a1d26fced7b5c259615
Manca <manca1@gmail.com>
2015-04-01 21:46:41 +0200

    Added main

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Algorithms: Design and Analysis, Part 2
+//
+//  Created by Nenad Mancevic on 4/1/15.
+//  Copyright (c) 2015 Nenad Mancevic. All rights reserved.
+//
+// Main program - all our assignments will be executed from here
+
+#include <iostream>
+
+#include "assignment1.hpp"
+
+int main(int argc, const char * argv[]) 
+{
+
+}
