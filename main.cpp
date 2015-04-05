6bce083145fd7ee06324a7f91de63dd691b752b1
Manca <manca1@gmail.com>
2015-04-05 15:47:59 +0200

    Main calls test case for Prim's algo

--- main.cpp
+++ main.cpp
@@ -16,6 +16,11 @@ int main(int argc, const char * argv[])
 	// Assignment 1
 	std::cout << "*** Assignment 1 - Scheduling problem ***" << std::endl;
 	assignment1::scheduling_problem::run_algorithm("data/jobs.txt");
-	std::cout << "*** Assignment 1 - Prim's algorithm ***" << std::endl;
+
+	std::cout << std::endl << "*** Assignment 1 - Prim's algorithm - Test Case 1 ***" << std::endl;
+	if (assignment1::prims_algorithm::test1())
+		std::cout << "... Test passed!" << std::endl;
+
+	std::cout << "*** Assignment 1 - Prim's algorithm - Main Test Case ***" << std::endl;
 	assignment1::prims_algorithm::run_algorithm("data/edges.txt");
 }
