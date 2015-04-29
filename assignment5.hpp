d27a88c22c5461dec290b2b2200afbbd1e937176
Manca <manca1@gmail.com>
2015-04-29 21:39:07 +0200

    Assignment5 - Traveling Salesman Problem added

---
+++ assignment5.hpp
@@ -0,0 +1,43 @@
+﻿#pragma once
+//
+//  assignment5.hpp
+//  Algorithms: Design and Analysis, Part 2
+//
+//  Programming Assignment 5
+//
+//  Created by Nenad Mancevic on 4/26/15.
+//  Copyright (c) 2015 Nenad Mancevic. All rights reserved.
+//
+//  Problem: Traveling Salesman Problem
+//
+/***********************
+In this assignment you will implement one or more algorithms for the 
+traveling salesman problem, such as the dynamic programming algorithm 
+covered in the video lectures. 
+Here is a data file (http://bit.ly/1bVhBE2) describing a TSP instance. 
+The first line indicates the number of cities. 
+Each city is a point in the plane, and each subsequent line indicates 
+the x- and y-coordinates of a single city.
+The distance between two cities is defined as the Euclidean distance 
+that is, two cities at locations (x,y) and (z,w) have distance 
+sqrt((x−z)^2+(y−w)^2) between them.
+
+In the box below, type in the minimum cost of a traveling salesman tour
+for this instance, rounded down to the nearest integer.
+
+OPTIONAL: If you want bigger data sets to play with, check out the 
+TSP instances from around the world here. The smallest data set 
+(Western Sahara) has 29 cities, and most of the data sets are 
+much bigger than that. What's the largest of these data sets that 
+you're able to solve --- using dynamic programming or, if you like, 
+a completely different method?
+
+HINT: You might experiment with ways to reduce the data set size. 
+For example, trying plotting the points. Can you infer any structure 
+of the optimal solution? Can you use that structure to speed up your algorithm?
+***********************/
+
+namespace assignment5
+{
+
+} // namespace
